e284b1d48d4befa4a06925a5b6df946fc8b90424
livmathot614-hue <livmathot614@gmail.com>
2026-05-17 09:27:46 -0700

    commit 73

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function getRecommendations() {
+
+  const food = document.getElementById("food");
+  const nature = document.getElementById("nature");
+  const art = document.getElementById("art");
+  const culture = document.getElementById("culture");
+  const hiking = document.getElementById("hiking");
+  const wellness = document.getElementById("wellness");
+  const entertainment = document.getElementById("entertainment");
+  const activities = document.getElementById("activities");
+  const adventures = document.getElementById("adventures");
+
+  console.log(food.checked);
+  console.log(nature.checked);
+  console.log(art.checked);
+  console.log(culture.checked);
+  console.log(hiking.checked);
+  console.log(wellness.checked);
+  console.log(entertainment.checked);
+  console.log(activities.checked);
+  console.log(adventures.checked);
+
+}
