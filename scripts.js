c2a05ba386221a1c05e03e4c38986e6ab9e47299
livmathot614-hue <livmathot614@gmail.com>
2026-05-17 13:06:01 -0700

    commit

--- scripts.js
+++ scripts.js
@@ -170,9 +170,9 @@ function loadRecommendations() {
 
     recommendations.innerHTML += `
       <div class="card">
-        <h2>Tokyo</h2>
-        <img src="./images/jazz.png" alt="Jazz Inn Uncle Tom">
-        <p>A cafe, with over 3,000 jazz records, is an authentic listening bar environment with great food.</p>
+        <h2>Tokyo Joypolis</h2>
+        <img src="./images/joypolis.png" alt="Tokyo Joypolis">
+        <p>A massive indoor amusment park, with VR rides, rollercoaster, and an arcade.</p>
       </div>
     `;
   }
